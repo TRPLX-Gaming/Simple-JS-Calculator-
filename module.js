bcaafee4a1a4528d8475f40806617c0b9ed0b302
TRPLX_Gaming <icgabriel2007@gmail.com>
2025-03-07 16:38:45 +0100

    Update module.js for game dev library testing

--- module.js
+++ module.js
@@ -3,3 +3,11 @@ export function greet() {
 }
 
 export const sth = true
+
+export function init(width,height) {
+  let can = document.createElement('canvas')
+  can.style.border = '3px black solid'
+  can.width = width
+  can.height = height
+  document.body.appendChild(can)
+}
